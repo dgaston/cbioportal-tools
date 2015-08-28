eff54e23585b8b132a7a9b0148f8aee2d12425e7
Dan Gaston <daniel.gaston@gmail.com>
2015-08-28 11:40:29 -0300

    Expression Profiling code

--- correlate_gene_expression.py
+++ correlate_gene_expression.py
@@ -1,9 +1,12 @@
 __author__ = 'dgaston'
 
-import cbioportal
 import argparse
 import csv
+import itertools
 
+from scipy import spearmanr
+
+import cbioportal
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -16,10 +19,27 @@ if __name__ == "__main__":
     with open(args.genes, 'rU') as genefile:
         genes = genefile.read().splitlines()
 
-    gene_list = ",".join(genes)
+    gene_list = "+".join(genes)
 
-    with open (args.output, 'w') as outfile:
-        with open (args.cases, 'rU') as casefile:
+    with open(args.output, 'w') as outfile:
+        outfile.write("Study\tCase List\tProfile\tGenes\tNumbers\tSpearman's Rho\tP-Value\n")
+        with open(args.cases, 'rU') as casefile:
             reader = csv.reader(casefile, dialect='excel-tab')
             for row in reader:
                 profile_data = cbioportal.get_multi_gene(row[1], row[2], gene_list)
+                header = profile_data.pop(0)
+                for pair in itertools.combinations(profile_data, 2):
+                    gene1 = pair[0].pop(0)
+                    gene2 = pair[1].pop(0)
+                    expression1 = list()
+                    expression2 = list()
+
+                    for values in itertools.izip(expression1, expression2):
+                        if values[0] != 'NaN' and values[1] != 'NaN':
+                            expression1.append(float(values[0]))
+                            expression2.append(float(values[1]))
+
+                    rho, pvalue = spearmanr(expression1, expression2)
+                    outfile.write("%s\t%s\t%s\t%s - %s\t%s, %s\t%s\t%s\n" %
+                                  (row[0], row[1], row[2], gene1, gene2, len(expression1), len(expression2),
+                                   rho, pvalue))
