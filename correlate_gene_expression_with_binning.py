3705655eb3afda9c96c51e88da9067f1c62ca229
Dan Gaston <daniel.gaston@gmail.com>
2016-02-10 09:34:33 -0400

    Updates

--- correlate_gene_expression_with_binning.py
+++ correlate_gene_expression_with_binning.py
@@ -14,7 +14,9 @@ if __name__ == "__main__":
     parser.add_argument('-c', '--cases', help="Input file with study, case, and profile ids [Required]")
     parser.add_argument('-g', '--genes', help="Text file with list of genes to evaluate for correlations. The first "
                                               "listed gene will control the binning")
-    parser.add_argument('-o', '--output', help='Output file name')
+    parser.add_argument('-o', '--output', help='Output file name. Will overwrite existing.')
+    parser.add_argument('-e', '--expression', help='File to write expression values per case for the binning control '
+                                                   'gene. Opens in append-mode so delete if already exists')
 
     args = parser.parse_args()
 
@@ -25,11 +27,11 @@ if __name__ == "__main__":
     prime_gene = genes[0]
 
     with open(args.output, 'w') as outfile:
-        outfile.write("Study\tCase List\tProfile\tGenes\t"
-                      "Bin1 #\tBin1 R\tBin1 p\t"
+        outfile.write("Study\tCase List\tProfile\tGenes\tMin Exp\t25th Pctl\tMedian\t75th Pctl\tMax\t"
+                      "Bin1 (Low) #\tBin1 R\tBin1 p\t"
                       "Bin2 #\tBin2 R\tBin2 p\t"
                       "Bin3 #\tBin3 R\tBin3 p\t"
-                      "Bin4 #\tBin4 R\tBin4 p\n")
+                      "Bin4 (High) #\tBin4 R\tBin4 p\n")
         with open(args.cases, 'rU') as casefile:
             reader = csv.reader(casefile, dialect='excel-tab')
             reader.next()
@@ -67,8 +69,17 @@ if __name__ == "__main__":
                     sys.stderr.write("WARNING: No Expression values for data set. Skipping\n")
                     continue
 
+                with open(args.expression, 'a') as expressionfile:
+                    expressionfile.write("{}\t{}\t{}".format(row[0], row[1], row[2]))
+                    for e in data:
+                        if e != 'NaN':
+                            expressionfile.write("\t{}".format(e))
+                    expressionfile.write("\n")
+
                 # Analyse the expression distribution of the control gene and assign samples to expression bins
                 sorted_primary = np.asarray(sorted(primary_data))
+                minimum = sorted_primary[0]
+                maximum = sorted_primary[-1]
                 lower = np.percentile(sorted_primary, 25)
                 mid = np.percentile(sorted_primary, 50)
                 upper = np.percentile(sorted_primary, 75)
@@ -113,8 +124,11 @@ if __name__ == "__main__":
                                 sys.stderr.write("ERROR: Value {val} did not fit within binned "
                                                  "quartiles\n".format(val=values[0]))
 
-                    outfile.write("{s}\t{c}\t{p}\t{g1} - {g2}\t".format(s=row[0], c=row[1], p=row[2],
-                                                                        g1=prime_gene, g2=comp_gene))
+                    outfile.write("{s}\t{c}\t{p}\t{g1} - {g2}\t"
+                                  "{min}\t{t1}\t{med}\t{t3}\t{max}".format(s=row[0], c=row[1], p=row[2],
+                                                                           g1=prime_gene, g2=comp_gene,
+                                                                           min=minimum, t1=lower, med=mid, t3=upper,
+                                                                           max=maximum))
                     if len(expression1_1) > 0 and len(expression2_1) > 0:
                         rho, pvalue = scipy.stats.spearmanr(expression1_1, expression2_1)
                         outfile.write("{}, {}\t{}\t{}\t".format(len(expression1_1), len(expression2_1), rho, pvalue))
